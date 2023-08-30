e94e6a5f64c3c7af8f89e089bca13dbf94d16d14
Kiameg <kiax1262@stud.kea.dk>
2023-08-30 12:20:43 +0200

    yes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const ulPointer = document.querySelector("ul");
+const header = "  <li><strong>Type</strong></li><li><strong>Fuel</strong></li><li><strong>Passengers</strong></li><li><strong>Stops</strong></li><li><strong>OwnedBy</strong></li><li><strong>Electric</strong></li><li><strong>Tandem</strong></li>";
 const vehicles = [
   { type: "Bus", fuel: "Diesel", passengers: 45, stops: ["Nørrebrogade", "Elmegade"] },
   { type: "Bil", fuel: "Benzin", passengers: 4, ownedBy: "Klaus" },
@@ -11,12 +13,61 @@ const vehicles = [
   { type: "Løbehjul", passengers: 1, isElectric: true },
 ];
 
-const ulPointer = document.querySelector("ul");
-const header = "<li><strong>Type</strong></li><li><strong>Fuel</strong></li><li><strong>Passengers</strong></li>";
+document.querySelectorAll("button").forEach((btn) => {
+  btn.addEventListener("click", btn_klik);
+});
+
+function btn_klik(evt) {
+  console.log(evt.target.dataset.filter);
+
+  if (evt.target.dataset.filter === "all") {
+    showTheseVehicles(vehicles);
+  } else {
+    let whatFilterFunc;
+    if (evt.target.dataset.filter === "electric_vehicles") {
+      whatFilterFunc = onlyElectricVehicles;
+    } else if (evt.target.dataset.filter === "electric_vehicles_owned_J") {
+      whatFilterFunc = electricOwnedByJonas;
+    } else if (evt.target.dataset.filter === "vehicles_more_than_two_s") {
+      whatFilterFunc = onlyTwoSeatsFilter;
+    }
+    showTheseVehicles(vehicles.filter(whatFilterFunc));
+  }
+}
+
+const vehiclesThatAreEletric = vehicles.filter(onlyElectricVehicles);
+
+function onlyElectricVehicles(vehicle) {
+  if (vehicle.isElectric) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const vehiclesWMoreThan2Seats = vehicles.filter(onlyTwoSeatsFilter);
+
+function onlyTwoSeatsFilter(vehicle) {
+  if (vehicle.passengers > 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const vehiclesOwnedByJ = vehicles.filter(electricOwnedByJonas);
+
+function electricOwnedByJonas(vehicle) {
+  if (vehicle.isElectric && vehicle.ownedBy === "Jonas") {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 showTheseVehicles(vehicles);
 
 function showTheseVehicles(arr) {
+  ulPointer.innerHTML = "";
   arr.forEach((each) => {
     ulPointer.innerHTML += `<li>${each.type}</li>`;
     ulPointer.innerHTML += `<li>${each.fuel}</li>`;
